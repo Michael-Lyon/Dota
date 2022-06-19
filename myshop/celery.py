da80b5d2ad8dd47028ab4acabff8e4f10da6d508
Michael Lyon <michaellyon@localhost.localdomain>
2022-06-19 03:43:24 +0100

    Celery test3

--- myshop/celery.py
+++ myshop/celery.py
@@ -5,7 +5,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
-app = Celery('myshop', broker='redis-12153.c257.us-east-1-3.ec2.cloud.redislabs.com:12153')
+app = Celery('myshop', broker='amqps://bccqdxkt:id-B_tDR0ilKms9CSPR0PRTqvjIhge2K@woodpecker.rmq.cloudamqp.com/bccqdxkt')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
