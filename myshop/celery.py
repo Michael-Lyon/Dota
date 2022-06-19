a4d9598d91bd98285cbc871930b06840cc2b2f1a
Michael Lyon <michaellyon@localhost.localdomain>
2022-06-19 03:35:18 +0100

    Celery test1

--- myshop/celery.py
+++ myshop/celery.py
@@ -1,11 +1,11 @@
 import os
 from celery import Celery
-
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
-app = Celery('myshop')
+app = Celery('myshop', backend=settings.REDIS_HOST, backend=settings.REDIS_HOST)
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
