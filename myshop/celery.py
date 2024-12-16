76aa31d241d00ddad5a1bfb1ec1dbc0dee7857a2
PyGod <mickiasomg@gmail.com>
2024-12-16 17:04:44 +0100

    xhtml2pdf integration

--- myshop/celery.py
+++ myshop/celery.py
@@ -1,13 +1,13 @@
-import os
+# import os
 
-from celery import Celery
-from django.conf import settings
+# from celery import Celery
+# from django.conf import settings
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+# # set the default Django settings module for the 'celery' program.
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
-app = Celery('myshop', broker='redis://137803e4-8dbd-4742-bd1a-a30600ec7c16@us1-singular-goldfish-38274.upstash.io')
-# app = Celery('myshop', broker='amqps://bccqdxkt:id-B_tDR0ilKms9CSPR0PRTqvjIhge2K@woodpecker.rmq.cloudamqp.com/bccqdxkt')
+# app = Celery('myshop', broker='redis://137803e4-8dbd-4742-bd1a-a30600ec7c16@us1-singular-goldfish-38274.upstash.io')
+# # app = Celery('myshop', broker='amqps://bccqdxkt:id-B_tDR0ilKms9CSPR0PRTqvjIhge2K@woodpecker.rmq.cloudamqp.com/bccqdxkt')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.autodiscover_tasks()
